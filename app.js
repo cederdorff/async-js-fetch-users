1e072dfddef5ade4c37d136211aa7d3620e10162
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2024-05-24 10:26:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-const url = "https://raw.githubusercontent.com/cederdorff/mdu-frontend/main/data/users.json";
+const url = "https://cederdorff.com/race/data/users.json";
 
 async function getUsers() {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 }
 
 function displayUsers(userList) {
-    let html = "";
-    for (const user of userList) {
-        html += /*html*/ `
+  let html = "";
+  for (const user of userList) {
+    html += /*html*/ `
             <article>
                 <img src="${user.image}">
                 <h2>${user.name}</h2>
                 <p>${user.title}</p>
             </article>`;
-    }
-    document.querySelector("#content").insertAdjacentHTML("afterbegin", html);
+  }
+  document.querySelector("#content").insertAdjacentHTML("afterbegin", html);
 }
 
 async function initApp() {
-    const users = await getUsers();
-    displayUsers(users);
+  const users = await getUsers();
+  displayUsers(users);
 }
 
 initApp();
